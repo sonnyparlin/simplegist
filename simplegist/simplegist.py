fa0dc969f605626717d666804f8613213b678e56
Sonny Parlin <sonnyparlin@gmail.com>
2021-01-31 14:37:58 -0500

    Update simplegist.py
    Added dots before module names

--- simplegist/simplegist.py
+++ simplegist/simplegist.py
@@ -1,11 +1,11 @@
 import requests
 import json
 
-from config import USERNAME, API_TOKEN, BASE_URL, GIST_URL
+from .config import USERNAME, API_TOKEN, BASE_URL, GIST_URL
 
-from mygist import Mygist
-from do import Do
-from comments import Comments
+from .mygist import Mygist
+from .do import Do
+from .comments import Comments
 
 class Simplegist:
 	"""
